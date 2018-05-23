6d2ce58a0867f9f646dadf0a30b7a297a075c73b
MatteoDP <mdp96ita@gmail.com>
2018-05-23 23:13:05 +0200

    DBConnection creato

---
+++ LibraryServer/src/dao/DBConnection.java
@@ -0,0 +1,30 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+	
+	public static Connection getConnection() {
+	   
+    	Connection connection = null;
+    	try {
+    		Class.forName("com.mysql.cj.jdbc.Driver");
+    	} catch (ClassNotFoundException e) {
+    		return connection;
+    	}
+
+    	try {
+    		connection = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/biblioteca?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","bibliotecario", "libriantichi");
+
+    	} catch (SQLException e) {
+    		return connection;
+    	}
+
+		return connection;
+		
+	}
+
+}
+
--- LibraryServer/src/dao/UserAuthtenticationQuesySet.java
+++ LibraryServer/src/dao/UserAuthtenticationQuesySet.java
@@ -1,12 +1,49 @@
 package dao;
 
-public class UserAuthtenticationQuesySet {
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import dao.DBConnection;
+
+public class UserAuthenticationQuerySet {
+	
+	private Connection con;
+	
+	public UserAuthenticationQuerySet(){
+        con = DBConnection.getConnection();
+	}
+	
 	
 	public void registration() {
 		
 	}
 	
-	public void login() {
+	public String login(String usr, String psw) throws SQLException {
+
+		    Statement stmt = null;
+			
+		   String query = "select id from user where username='"+usr+"' and passwd='"+psw+"'";
+			try {
+		        stmt=con.createStatement();
+		        ResultSet rs = stmt.executeQuery(query);
+		        	if(rs.next()) {
+		            String id = rs.getString("id");
+		           
+		        	return id;
+		        	}else return "Authentication Failed";
+		       
+		    } catch (SQLException e ) {
+		    	
+		    } finally {
+		        if (stmt != null) { stmt.close(); }
+		    }
+			return null;
 		
 	}
 
