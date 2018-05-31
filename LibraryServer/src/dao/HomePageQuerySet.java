bca7eecbd9b78641c864c441eafc5258607af7f7
Marco Ovidi <marco.ovidi.96@gmail.com>
2018-05-31 16:07:25 +0200

    aggiornamento

--- LibraryServer/src/dao/HomePageQuerySet.java
+++ LibraryServer/src/dao/HomePageQuerySet.java
@@ -183,7 +183,77 @@ public class HomePageQuerySet {
 	//TODO serve un metodo che recupera le singole pagine assegnate ?
 	
 	// !!! questione dei segnalibri !!!  id documento, url immagine, nome del testo,
-	public void loadMyCollectionList() {
+	public LinkedList<String[]> loadMyCollectionList(UUIDUser usr) {
+		//CONNESSIONE
+  	  	//STEP 1 parametri di connessione   
+  		// JDBC driver name and database URL
+  		final String JDBC_DRIVER = DBConnection.getJdbcDriver();
+  		final String DB_URL = DBConnection.getDbUrl();
+
+  		//  Database credentials
+  		final String USER = DBConnection.getUser();
+  		final String PASS = DBConnection.getPassword();
+				
+  		Connection conn = null;
+  		Statement stmt = null;
+  		
+  		//oggetti di ausilio
+  		LinkedList<String[]> myColl = new LinkedList<String[]>();
+  		
+  		try{
+  			//STEP 2: Register JDBC driver
+  			Class.forName(JDBC_DRIVER);
+  			
+  			//STEP 3: Open a connection
+  			conn = DriverManager.getConnection(DB_URL,USER,PASS);
+  			
+  			//STEP 4: Execute a query
+  			stmt = conn.createStatement();
+  			String sql;
+  			sql = "SELECT p.ID_document, p.image,d.title "
+  				  + "FROM personal_collection as pc join page as p join document as d "
+  				  + "on pc.ID_document=p.ID_document and pc.ID_document=d.ID "
+  				  + "where pc.ID_user= " + usr.getValue() + "and p.number=1;"; 
+  			
+  			ResultSet rs = stmt.executeQuery(sql);
+						
+  			//STEP 5: Extract data from result set
+						
+  			//Retrieve by column name
+  			while(rs.next()) {
+  				
+  				myColl
+  				
+  			}  
+  			//STEP 6: Clean-up environment
+  			rs.close();
+  			stmt.close();
+  			conn.close();
+  		}catch(SQLException se){
+  			//Handle errors for JDBC
+  			se.printStackTrace();
+  		}catch(Exception e){
+  			//Handle errors for Class.forName
+  			e.printStackTrace();
+  		}finally{
+  			//finally block used to close resources
+  			try{
+  				if(stmt!=null)
+  					stmt.close();
+						}catch(SQLException se2){
+						}//nothing we can do
+  			try{	
+  				if(conn!=null)
+  					conn.close();
+  			}catch(SQLException se){
+  				se.printStackTrace();
+  			}//end finally try	
+  		}//end try	
+  		return swp;
+		
+	}
+	
+	public void loadMyCollectionMetaData() {
 		
 	}
 	
