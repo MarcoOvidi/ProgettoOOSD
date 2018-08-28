3bd734960b2add1deaf56adf638785fbd8381289
MatteoDP <mdp96ita@gmail.com>
2018-08-28 15:59:56 +0200

    commit

--- LibraryServer/src/controller/LoginController.java
+++ LibraryServer/src/controller/LoginController.java
@@ -1,4 +1,5 @@
 package controller;
+
 import fx_view.LoginScene;
 import java.io.IOException;
 import java.sql.SQLException;
@@ -12,8 +13,25 @@ import javax.servlet.http.HttpSession;
 import dao.DatabaseException;
 import dao.UserAuthenticationQuerySet;
 import fx_view.SceneController;
+<<<<<<< HEAD
 import vo.UUIDUser;
 import fx_view.UserID;
+=======
+import model.User;
+import vo.UUIDUser;
+import controller.SessionDataHandler;
+
+public class LoginController {
+	
+	/**
+	 * 
+	 * @param lscene
+	 * @param usr nome utente
+	 * @param psw password
+	 * @return instanza User corrispondente alle credenziali valide di login, null se invalide
+	 */
+	public static User login(LoginScene lscene, String usr, String psw) throws DatabaseException {
+		lscene.displayMessage("Verifico identitï¿½...");
 
 public class LoginController{
 	public static void login(LoginScene lscene, String usr, String psw) {
@@ -35,17 +53,32 @@ public class LoginController{
 				// TODO Auto-generated catch block
 					lscene.displayMessage("Ops qualcosa è andato storto");
 				e.printStackTrace();
+		try {
+			UUIDUser id = loginDB(usr, psw);
+			if (id != null) {
+				SceneController.loadScene("home");
+				return SessionDataHandler.GetInstance().getUser(id);
+			} else {
+				lscene.displayMessage("Username o/e password non validi");
+				return null;
 			}
-			
-			
-		} catch (SQLException e) {
+		} catch (DatabaseException e) {
 			// TODO Auto-generated catch block
-			lscene.displayMessage("Ops qualcosa è andato storto");
-			e.printStackTrace();//
+			lscene.displayMessage("Ops qualcosa ï¿½ andato storto");
+			throw e;
 		}
-		
 
 	}
+
+	public static UUIDUser loginDB(String usr, String psw) throws DatabaseException {
+		UUIDUser id;
+		try {
+			id = UserAuthenticationQuerySet.login(usr, psw);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			id = null;
+		}
+		return id;
+	}
 }
-
-
