0e967371d89dbd0478b18a0ab211881533acd91b
MatteoDP <mdp96ita@gmail.com>
2018-11-24 13:22:06 +0100

    # WARNING: head commit changed in the meantime
    up

--- LibraryServer/src/fx_view/NewDocument.java
+++ LibraryServer/src/fx_view/NewDocument.java
@@ -145,6 +145,7 @@ public class NewDocument {
 
 	@FXML
 	public void caseTwo() {
+		description=inarea.getText();
 		label.setText("Riguardo la composizione dell'opera si conosce");
 		System.out.println(c);
 		next.setVisible(false);
