b71ec88a56621de9953f2126281030e8b6328472
Marco Ovidi <marco.ovidi.96@gmail.com>
2018-11-26 18:45:55 +0100

    modifiche adminc ontroller

--- LibraryServer/src/controller/AdministrationController.java
+++ LibraryServer/src/controller/AdministrationController.java
@@ -12,35 +12,19 @@ import vo.UUIDUser;
 
 public class AdministrationController {
 
-	private HashMap<UUIDRequest, String> pendingRequests = new HashMap<UUIDRequest, String>();
-	private HashMap<UUIDRequest, String> readRequests = new HashMap<UUIDRequest, String>();
 
-	public HashMap<UUIDRequest, String> getPendingRequests() {
-		return pendingRequests;
-	}
-
-	public void setPendingRequests(HashMap<UUIDRequest, String> pendingRequests) {
-		this.pendingRequests = pendingRequests;
-	}
-
-	public void loadPendingRequestList() {
+	public LinkedList<Request> getRequest(Integer i){
+		LinkedList<Request> req = null;
 		try {
-			pendingRequests = AdministratorQuerySet.loadRequestsList(0);
-		} catch (DatabaseException e) {
+			req = AdministratorQuerySet.loadRequestsList(i);
+		}catch(Exception e) {
 			e.printStackTrace();
 			System.out.println(e.getMessage());
 		}
+		
+		return req;
 	}
-
-	public void loadReadRequestsList() {
-		try {
-			setReadRequests(AdministratorQuerySet.loadRequestsList(1));
-		} catch (DatabaseException e) {
-			e.printStackTrace();
-			System.out.println(e.getMessage());
-		}
-	}
-
+	
 	public Request loadRequest(UUIDRequest id) {
 		Request r = null;
 		try {
@@ -70,14 +54,6 @@ public class AdministrationController {
 		}
 
 		return res;
-	}
-
-	public HashMap<UUIDRequest, String> getReadRequests() {
-		return readRequests;
-	}
-
-	public void setReadRequests(HashMap<UUIDRequest, String> readRequests) {
-		this.readRequests = readRequests;
 	}
 
 }
